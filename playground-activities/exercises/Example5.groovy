f25001379c38ad50043df3e74eaae5ea0f6e3859
Artur Boronat <artur.boronat@leicester.ac.uk>
2024-05-29 16:49:47 +0100

    Add demo of activity inspection

---
+++ playground-activities/exercises/Example5.groovy
@@ -0,0 +1,24 @@
+package flowchartToHtmlExamples
+import static yamtl.dsl.Rule.*
+import org.eclipse.emf.ecore.EPackage
+import yamtl.core.YAMTLModule
+import yamtl.groovy.YAMTLGroovyExtensions
+import yamtl.groovy.YAMTLGroovyExtensions_dynamicEMF
+
+class Example5 extends YAMTLModule {
+	public Example5(EPackage flowchartPk, EPackage htmlPk) {
+		YAMTLGroovyExtensions_dynamicEMF.init(this)
+
+		header().in("in", flowchartPk).out("out", htmlPk)
+
+		ruleStore([
+            //TODO: Create a lazy rule
+
+            //TODO: Create a non-lazy/regular rule
+
+            //TODO: Create a rule that fetches the outputs of the first two rules
+		])
+
+	}
+
+}
